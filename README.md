fae89ab51910e9f5289c63b3a6fbdff8ce83f8f9
Tor Røttum <tor@rottum.me>
2016-09-07 10:46:47 +0200

    added workflow and src

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # alfred-passwordstore
-Pass workflow for Alfred
+[Pass](https://www.passwordstore.org/) workflow for Alfred with fuzzy matching
+
+# Credits
+Icon made by [Pixel Buddha](http://www.flaticon.com/authors/pixel-buddha) from [flaticon.com](http://www.flaticon.com) is licensed by [Creative Commons BY 3.0](http://creativecommons.org/licenses/by/3.0/)
---
+++ src/filter.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+import re
+import os
+import fnmatch
+import sys
+import json
+
+def fuzzyfinder(user_input, collection):
+    suggestions = []
+    pattern = '.*?'.join(user_input)
+    regex = re.compile(pattern)
+    for item in collection:
+        match = regex.search(item)
+        if match:
+            suggestions.append((len(match.group()), match.start(), item))
+    return [x for _, _, x in sorted(suggestions)]
+
+def create_item(path):
+    return {
+        'title': path,
+        'arg': path
+    }
+
+items = []
+for root, dirs, files in os.walk("/Users/tor/.password-store", topdown=False):
+    for name in files:
+        if fnmatch.fnmatch(os.path.join(root, name), '*.gpg'):
+            fullPath = os.path.join(root, name).replace('/Users/tor/.password-store/', '')
+            items.append(os.path.splitext(fullPath)[0])
+
+query = ' '.join(sys.argv[1:]).replace(' ', '/')
+
+items = fuzzyfinder(query, items)
+items = list(map(lambda path: {'title': path, 'arg': path}, items))
+
+print(json.dumps({
+    'items': items
+}))
